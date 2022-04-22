17c80810e8c1282fb9a84dfced9101b153a1593b
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-04-21 19:36:09 -0700

    Time: 147 ms (7.83%), Space: 49.6 MB (5.02%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+    // keep adding options to tree till both right and left are null
+    
+    let ans = []
+    let stack = []
+    
+    const getParen = (leftlen, rightLen) => {
+        if(leftlen === n && rightLen === n) {
+            const finalParen = stack.join('') 
+            console.log(stack)
+            ans.push(finalParen)
+            return
+        }
+        if(leftlen < n) {
+            stack.push('(')
+            getParen(leftlen + 1, rightLen)
+            stack.pop()
+        }
+        
+        if(rightLen < leftlen) {
+            stack.push(')')
+            getParen(leftlen, rightLen +1)
+            stack.pop()
+        }
+        
+    }
+    getParen(0, 0)
+    return ans
+    
+};
