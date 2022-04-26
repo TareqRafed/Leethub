e2c92d80bcb777878eb994dabd3a9e12ab6c3782
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-04-26 03:21:30 +0300

    Time: 56 ms (55.09%), Space: 14.1 MB (19.51%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        rowLength = len(obstacleGrid[0])
+        row = rowLength * [0]
+        noWay = False
+
+        
+        for i in range(len(obstacleGrid) - 1, -1, -1):
+            newRow = rowLength * [0]
+            for j in range(rowLength - 1, -1, -1):
+                if j + 1 == rowLength:
+                    if i == len(obstacleGrid) - 1:
+                        newRow[j] = 1
+                    if row[j] > 0:
+                        newRow[j] = 1 - obstacleGrid[i][j]
+
+                    #newRow[j] = 0
+                    continue;
+                
+                if obstacleGrid[i][j] is not 1:
+                    if obstacleGrid[i][j + 1] is not 1:
+                        newRow[j] = newRow[j + 1] + row[j]
+                    else:
+                        newRow[j] = row[j]
+                else:
+                    newRow[j] = 0
+                
+            noWay = noWay or all(path == 0 for path in newRow)
+            row = newRow
+        
+        ## in case the end has an obstacle, or there is a full row of obstacles
+        ## there is no way for the robot to reach the end
+
+        if obstacleGrid[len(obstacleGrid) - 1][rowLength - 1] == 1:
+            noWay = True
+        if noWay:
+            return 0
+        
+        return row[0]
+    
+
+
+
+
+[11, 5, 2, 1, 1]
+[6, 3, 1, 0, 0]
+[3, 2, 1, 0, 1]
+[1, 1, 1, 1, 1]
