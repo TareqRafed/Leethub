6d9d60101fb93051bfde340dd4951c162dcc84d4
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-05-28 23:30:54 +0300

    Time: 33 ms (92.98%), Space: 14.2 MB (83.67%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -7,29 +7,21 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        ans = defaultdict(list)
-        def bfs(root, rootSib, lvl = 0):
-            if not root and not rootSib:
-                return
+        qu = collections.deque()
+        qu.append(root)
+        ans = []
+        while qu:
+            lenQ = len(qu)
+            lvl = []
+            for i in range(lenQ):
+                node = qu.popleft()
+                
+                if node:
+                    lvl.append(node.val)
+                    qu.append(node.left)
+                    qu.append(node.right)
+            if lvl:
+                ans.append(lvl)
             
-            if root and not rootSib:
-                ans[lvl].append(root.val)
-                bfs(root.left, root.right, lvl + 1)
-                return
-            
-            if rootSib and not root:
-                ans[lvl].append(rootSib.val)
-                bfs(rootSib.left, rootSib.right, lvl + 1)
-                return
-            
-            ans[lvl].append(root.val)
-            ans[lvl].append(rootSib.val)
-            
-            bfs(root.left, root.right, lvl + 1)
-            bfs(rootSib.left, rootSib.right, lvl + 1)
-            
-            
-            
-            
-        bfs(root, None)
-        return ans.values()
+        
+        return ans
