8c71a898ac19fbdf339aac28ea3f42a49d79930e
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-06-02 21:14:47 +0300

    Time: 325 ms (18.03%), Space: 88.6 MB (24.30%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,32 +6,22 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
         
-        def array_to_tree(left, right):
-            nonlocal preorder_index
-            # if there are no elements to construct the tree
-            if left > right: return None
-
-            # select the preorder_index element as the root and increment it
-            root_value = preorder[preorder_index]
-            root = TreeNode(root_value)
-
-
-            preorder_index += 1
-
-            # build left and right subtree
-            # excluding inorder_index_map[root_value] element because it's the root
-            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
-            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
-
+        def treeToArray(pre, ino):
+            #print(pre, ino)
+            if not pre or not ino:
+                return None
+            
+            mid_val = pre[0]
+            pre_index = ino.index(mid_val)
+            
+            root = TreeNode(mid_val)
+            
+            root.left = treeToArray(pre[1:pre_index + 1], ino[:pre_index])
+            root.right = treeToArray(pre[pre_index + 1:], ino[pre_index + 1:])
+            #print(root)  
             return root
-
-        preorder_index = 0
-
-        # build a hashmap to store value -> its index relations
-        inorder_index_map = {}
-        for index, value in enumerate(inorder):
-            inorder_index_map[value] = index
-
-        return array_to_tree(0, len(preorder) - 1)
+            
         
+        return treeToArray(preorder, inorder)
