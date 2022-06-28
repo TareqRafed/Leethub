bb0b527b25c1b8cfcae4f2a11b833f847f428cf2
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-06-29 00:31:04 +0300

    Time: 337 ms (33.72%), Space: 14.8 MB (52.19%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,18 +1,26 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        word = sorted(s)
-        co = Counter(word) 
-        vis = set()
-        res = 0
-        for key in co.keys():
-            val = co[key]
-            while val in vis and val > 0:
-                res += 1
-                val -= 1
-            if val != 0:
-                vis.add(val)
         
-        return res
+        freq = [0] * 26
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+        
+        pq = [-f for f in freq if f != 0]
+        
+        heapq.heapify(pq)
+        
+        delete_count = 0
+        
+        while len(pq) > 1:
+            top_e = -heapq.heappop(pq)
             
+            if top_e == -pq[0]:
+                if top_e - 1 > 0:
+                    top_e -= 1
+                    heapq.heappush(pq, - top_e)
+                
+                delete_count += 1
+        
+        return delete_count
         
         
