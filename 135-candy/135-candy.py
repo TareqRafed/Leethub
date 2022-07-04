cd3eb3d0aa668bd25063362ba3bbc38904c5a4ad
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-07-04 08:31:14 +0300

    Time: 284 ms (34.28%), Space: 16.7 MB (96.79%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        ans = len(ratings)
+        kidsCandies = [1 for _ in ratings]
+        
+        
+        for i in range(1, len(ratings)):
+            if  ratings[i - 1] < ratings[i]:
+                kidsCandies[i] += kidsCandies[i - 1]
+        
+        for i in range(len(ratings) - 1, 0, -1):
+            if ratings[i - 1] > ratings[i] and kidsCandies[i - 1] <= kidsCandies[i]:
+                kidsCandies[i - 1] += (kidsCandies[i] - kidsCandies[i - 1]) + 1
+        print(kidsCandies)
+        return sum(kidsCandies)
