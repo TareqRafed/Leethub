16ef884157250331e16ba166234b20096cbe3419
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-07-06 15:15:06 +0300

    Time: 894 ms (19.49%), Space: 16.6 MB (33.89%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -6,18 +6,23 @@ class Solution:
         for source, dist, wei in times:
             edges[source].append((dist, wei))
         
-        queue = collections.deque([(0, k)])
+        heap = [(0, k)]
         visit = set()
-        t = [0] + [float('inf')] * n
-        while queue:
-            weight, source = queue.popleft()
-
-            if weight < t[source]:    
-                t[source] = weight
-                for dist, wei in edges[source]:
-                    queue.append((wei + weight, dist))
-        
-        return max(t) if max(t) != float('inf') else -1
+        t = 0
+        while heap:
+            weight, source = heapq.heappop(heap)
+            
+            if source in visit:
+                continue
+            
+            visit.add(source)
+            
+            t = max(t, weight)
+            
+            for dist, wei in edges[source]:
+                heapq.heappush(heap, (wei + weight, dist))
+    
+        return t if len(visit) == n else -1
             
         
 
