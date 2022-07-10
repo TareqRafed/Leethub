c508608dcc0f8d5df727fafb98e30dc97c441c12
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2022-07-10 12:13:38 +0300

    Time: 67 ms (64.37%), Space: 13.8 MB (76.17%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,55 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hmap = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+        res, prev, dic = 0, 0, { 
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
         }
         
-        ans = 0 
-        i = 0
-        while i < len(s):
-            
-            if (s[i] == 'I' or s[i] == 'X' or s[i] == 'C') and i + 1 < len(s):
-                
-                if s[i] == 'I':
-                    if s[i + 1] == 'V':
-                        ans += 4
-                        i += 2
-                        continue
-
-                    if s[i + 1] == 'X':
-                        ans += 9
-                        i += 2
-                        continue
-                
-                if s[i] == 'X':
-                    if s[i + 1] == 'L':
-                        ans += 40
-                        i += 2
-                        continue
-
-                    if s[i + 1] == 'C':
-                        ans += 90
-                        i += 2
-                        continue
-                    
-                if s[i] == 'C':
-                    if s[i + 1] == 'D':
-                        ans += 400
-                        i += 2
-                        continue
-
-                    if s[i + 1] == 'M':
-                        ans += 900
-                        i += 2
-                        continue
-                
-            ans += hmap[s[i]]
-            i += 1
-        
-        return ans
+        for i in s[::-1]:
+            if dic[i] >= prev:
+                res += dic[i]
+            else:
+                res -= dic[i]
+            prev = dic[i]
+        return res
