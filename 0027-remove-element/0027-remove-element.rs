d7b1e6098fc2367f648148daede70a06d7dbb238
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2024-03-16 12:02:47 +0100

    Time: 1 ms (79.16%), Space: 2.1 MB (34.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.rs
+++ 0027-remove-element/0027-remove-element.rs
@@ -1,20 +1,17 @@
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut res: i32 = 0;
-        let mut arr_len: i32 = nums.len() as i32;
-        let mut temp: Vec<usize> = Vec::new();
+        let mut j: usize = nums.len(); 
+        let mut i: usize = 0;
         
-        for (i, iter_v) in nums.iter().enumerate() {
-            if *iter_v == val {
-                res += 1;
-                temp.push(i);
+        while i < j {
+            if nums[i] == val {
+                nums.swap(i, j - 1);
+                j -= 1;
+                continue;
             }
+            i += 1
         }
-
-        for (ind, i_v) in temp.iter().enumerate() {
-            nums.remove(*i_v - ind);
-        }
-
-        arr_len - res
+        
+        i as i32
     }
 }
