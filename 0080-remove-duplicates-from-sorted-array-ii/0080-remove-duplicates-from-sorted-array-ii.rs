54843b8e7960cbd761de448ecfbf079f83d52a41
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2024-09-18 05:09:59 +0200

    Time: 1 ms (76.73%), Space: 2.2 MB (52.72%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.rs
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.rs
@@ -2,19 +2,13 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        let mut hm: HashMap<i32, i32> = HashMap::new();
-
-        nums.iter().for_each(|e| *hm.entry(*e).or_insert(0) += 1);
         let mut i: usize = 0;
-        while i < nums.len() {
-            let count = hm.get(&nums[i]).unwrap();
-            if *count > 2 {
-                *hm.get_mut(&nums[i]).unwrap() -= 1;
-                nums.remove(i);
-                continue;
+        for arrIndex in 0..nums.len() {
+            if i < 2 || nums[i-2] != nums[arrIndex] {
+                nums[i] = nums[arrIndex];
+                i += 1;
             }
-            i += 1;
         }
-        nums.len() as i32
+        i as i32
     }
 }
