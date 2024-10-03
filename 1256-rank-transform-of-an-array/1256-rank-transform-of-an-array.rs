16a21aee8a7437d328fce774f08399d77ce73824
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2024-10-03 18:29:41 +0200

    Time: 21 ms (56.41%), Space: 5.3 MB (10.26%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.rs
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.rs
@@ -1,14 +1,19 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn array_rank_transform(arr: Vec<i32>) -> Vec<i32> {
         let mut sorted = arr.clone();
         sorted.sort();
         sorted.dedup();
         let mut ans = arr.clone();
+        
+        let mut hm: HashMap<i32, i32> = HashMap::new();
+        for i in 0..sorted.len() {
+            hm.insert(sorted[i], i as i32 +1);
+        }
+
         for i in 0..arr.len() {
-            match sorted.binary_search(&arr[i]) {
-                Ok(index) => ans[i] = index as i32 + 1,
-                _ => println!("Err"),
-            }
+            ans[i] = hm[&arr[i]];
         }
         ans
     }
