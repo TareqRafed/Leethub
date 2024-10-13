fa9e8a1d3ffd4f589516a26fa19b49f747968271
Tariq Rafid <47465125+TareqRafed@users.noreply.github.com>
2024-10-13 20:41:03 +0200

    Time: 0 ms (100%), Space: 2.2 MB (63.16%) - LeetHub

--- 1983-maximum-population-year/1983-maximum-population-year.rs
+++ 1983-maximum-population-year/1983-maximum-population-year.rs
@@ -3,14 +3,14 @@ impl Solution {
         let mut years: Vec<i32> = vec![0; 2051];
         
         for log in &logs {
-            for year in log[0]..log[1] {
-                years[year as usize] += 1;
-            }
+            years[log[0] as usize] += 1;
+            years[log[1] as usize] -= 1;
         }
 
         let mut max_year = 1950;
         // get max index;
         for year in 1950..years.len() {
+            years[year] += years[year - 1];
             if years[max_year as usize] < years[year] {
                 max_year = year as i32;
             }
